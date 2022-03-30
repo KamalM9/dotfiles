956276aaf46cbad7fdcae3f93c552ffa58be51a6
kamal mustafa <kamal.mustafa.ks@gmail.com>
2022-03-30 12:30:30 +0200

    dotfiles draft

---
+++ init.vim
@@ -0,0 +1,45 @@
+call plug#begin()
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+Plug 'ms-jpq/coq_nvim', {'branch': 'coq'}
+Plug 'ms-jpq/coq.artifacts', {'branch': 'artifacts'}
+Plug 'tpope/vim-fugitive'
+Plug 'vim-airline/vim-airline'
+Plug 'arcticicestudio/nord-vim'
+Plug 'preservim/nerdcommenter'
+Plug 'tpope/vim-surround'
+call plug#end()
+
+" Set termguicolors
+"set nocompatible
+"if (has("termguicolors"))
+"  set termguicolors
+"endif
+syntax enable
+
+" Set colorscheme
+colorscheme nord
+
+" Find files using Telescope command-line sugar.
+nnoremap <leader>ff <cmd>Telescope find_files<cr>
+nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+nnoremap <leader>fb <cmd>Telescope buffers<cr>
+nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+
+" Telescope maps
+nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
+nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
+nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
+nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
+
+" Fugitive maps
+nmap <leader>gs :G<cr>
+nmap <leader>gj :diffget //3<cr>
+nmap <leader>gf :diffget //2<cr>
+
+" Auto-start COQnow
+augroup COQ 
+        autocmd!
+        autocmd VimEnter * COQnow -s
+augroup END
+
---
+++ install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+set -exu
+set -o pipefail
+
+# nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+sudo mv ./nvim.appimage /usr/bin/nvim
+
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --config vi
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --config vim
+
+# BurntSushi/ripgrep
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+sudo dpkg -i ripgrep_13.0.0_amd64.deb
+
+# sharkdp/fd
+sudo apt install fd-find
+
+#  ms-jpq/coq_nvim
+sudo apt install --yes -- python3-venv
+
+# install fonts
+FONT_HOME=~/.local/share/fonts
+
+echo "installing fonts at $PWD to $FONT_HOME"
+mkdir -p "$FONT_HOME/adobe-fonts/source-code-pro"
+
+(git clone \
+   --branch release \
+   --depth 1 \
+   'https://github.com/adobe-fonts/source-code-pro.git' \
+   "$FONT_HOME/adobe-fonts/source-code-pro" && \
+fc-cache -f -v "$FONT_HOME/adobe-fonts/source-code-pro") || echo fonts exist
+
+# install tmux
+sudo apt install libevent-dev ncurses-dev build-essential bison pkg-config
+curl -s https://api.github.com/repos/nelsonenzo/tmux-appimage/releases/latest \
+| grep "browser_download_url.*appimage" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi - \
+&& chmod +x tmux.appimage
+sudo mv tmux.appimage /usr/local/bin/tmux
+sudo cp .tmux.conf /home/$USER 
+
+# zsh plugins
+ZSH_CUSTOM="./.oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/jeffreytse/zsh-vi-mode ~/.oh-my-zsh/custom/plugins/zsh-vi-mode
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/popstas/zsh-command-time.git ~/.oh-my-zsh/custom/plugins/command-time
+
+# diff-so-fancy
+sudo add-apt-repository ppa:aos1/diff-so-fancy
+sudo apt update
+sudo apt install diff-so-fancy -y
+
+cp .config/nvim /home/$USER -R
