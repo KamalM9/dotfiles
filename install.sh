3f36febce1169330d202596816a831d29b81f3f6
Ubuntu <kamal@kamal.vbqmysktwpvepjbtr5aojpd3pd.xx.internal.cloudapp.net>
2022-03-30 20:19:29 +0000

    Update zsh and fix installation

--- install.sh
+++ install.sh
@@ -12,6 +12,10 @@ sudo update-alternatives --config vi
 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
 sudo update-alternatives --config vim
 
+# install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 # BurntSushi/ripgrep
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
 sudo dpkg -i ripgrep_13.0.0_amd64.deb
@@ -44,9 +48,10 @@ curl -s https://api.github.com/repos/nelsonenzo/tmux-appimage/releases/latest \
 | wget -qi - \
 && chmod +x tmux.appimage
 sudo mv tmux.appimage /usr/local/bin/tmux
-sudo cp .tmux.conf /home/$USER 
+sudo cp tmux.conf /home/$USER/.tmux.conf
 
 # zsh plugins
+sudo apt install zsh
 ZSH_CUSTOM="./.oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/jeffreytse/zsh-vi-mode ~/.oh-my-zsh/custom/plugins/zsh-vi-mode
@@ -58,4 +63,5 @@ sudo add-apt-repository ppa:aos1/diff-so-fancy
 sudo apt update
 sudo apt install diff-so-fancy -y
 
-cp .config/nvim /home/$USER -R
+sudo mkdir -p /home/$USER/.config/nvim
+sudo cp init.vim /home/$USER/.config/nvim
